5671ef2f13581c48431b704241a0b506e7ee134a
cloudste <stefano.bertotti@cloudspirit.be>
2023-04-27 15:03:35 +0200

    Updated bulk examples - Rev. 1

--- examples/bulk/main.tf
+++ examples/bulk/main.tf
@@ -3,15 +3,15 @@ terraform {
     aci = {
       #source =  "local/providers/aci"
       source =  "ciscodevnet/aci"
-      #version = "1.0.0"
+      #version = "2.6.1"
     }
   }
   required_version = ">= 0.13"
 }
 
 provider "aci" {
-  username = "admin"
-  password = "Redmoon0"
-  url      = "http://10.0.2.214"
+  username = ""
+  password = ""
+  url      = "http://"
   insecure = true
 }
